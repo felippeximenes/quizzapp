b73db706035625abf54d9f243c86579aa117f79c
Felippe <felippelpximenes@outlook.com>
2024-11-28 21:28:49 -0300

    algumas alterações da pagina de assuntos

---
+++ pages/quiz/quiz.js
@@ -0,0 +1,26 @@
+import { verificarTema, trocarTema } from "../../helpers/tema-helper.js"
+
+const botaoTema = document.querySelector(".tema button")
+const body = document.querySelector("body")
+
+botaoTema.addEventListener("click", () => {
+    trocarTema(body, botaoTema)
+})
+
+verificarTema(body, botaoTema)
+
+const assunto = localStorage.getItem("assunto")
+
+function alterarAssunto() {
+    const divIcone = document.querySelector(".assunto_icone")
+    const iconeImg = document.querySelector(".assunto_icone img")
+    const assuntoTitulo = document.querySelector(".assunto h1")
+
+    divIcone.classList.add(assunto.toLowerCase())
+    
+    assuntoTitulo.innerText = assunto
+    
+
+}
+
+alterarAssunto() 
